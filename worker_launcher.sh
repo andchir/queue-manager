8985bb6cd65e60aeeefb879fbea9c376adc03801
Andrei Chirko <andchir@gmail.com>
2024-04-18 15:35:23 +0300

    worker launcher fix

--- worker_launcher.sh
+++ worker_launcher.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
 BASEDIR=$(dirname $0)
-source "${BASEDIR}/venv/bin/activate"
-python "${BASEDIR}/worker_launcher.py" python 2 workers/stable-diffusion-webui.py
+cd $BASEDIR
+source "venv/bin/activate"
+python "worker_launcher.py" python 2 workers/stable-diffusion-webui.py
