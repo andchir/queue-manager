48c78b0bf9bd79d4f01ccb362ba664af6e9ef6e7
andrew <andchir@gmail.com>
2024-05-02 00:26:36 +0300

    add CORSMiddleware

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from routes import router as api_router
 from config import settings
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title=settings.app_name, swagger_ui_parameters={'persistAuthorization': True})
 app.include_router(api_router)
+
+origins = [
+    'http://localhost',
+    'http://localhost:8001',
+    'http://localhost:4200',
+    'https://api2app.ru'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
