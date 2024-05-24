9054d660136daece1fe8f6006398eb02dc464679
Andrei Chirko <andchir@gmail.com>
2024-05-24 16:10:22 +0300

    add code-farmer worker

--- workers/code-former.sh
+++ workers/code-former.sh
@@ -9,6 +9,9 @@ echo "DIR_PATH: ${DIR_PATH}"
 echo "BASE_NAME: ${BASE_NAME}"
 
 python "/home/andrew/PycharmProjects/CodeFormer/inference_codeformer.py" \
--w 0.7 --input_path "$1" \
---bg_upsampler realesrgan --face_upsample \
+--input_path "$1" \
+--upscale 2 \
+--fidelity_weight 0.9 \
+--bg_upsampler realesrgan \
+--face_upsample \
 --output_path "${DIR_PATH}/output"
