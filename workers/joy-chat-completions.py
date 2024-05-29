39caab0059b7babd1f724e0dbcd86c3d40036e0d
Andrei Chirko <andchir@gmail.com>
2024-05-29 19:02:30 +0300

    joy worker upd

--- workers/joy-chat-completions.py
+++ workers/joy-chat-completions.py
@@ -13,13 +13,55 @@ from utils.queue_manager import get_queue_next, send_queue_error, send_queue_res
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+def chat_completion(question):
+    url = 'http://localhost:1337/v1/chat/completions'
+
+    payload = {
+        'messages': [{'content': 'Hello!', 'role': 'user'}],
+        'model': 'openchat-3.5-7b',
+        'stream': False,
+        'max_tokens': 2048,
+        'frequency_penalty': 0,
+        'presence_penalty': 0,
+        'temperature': 0.7,
+        'top_p': 0.95
+    }
+
+    res = None
+    try:
+        response = requests.post(url=url, json=payload)
+        res = response.json()
+    except Exception as e:
+        print(str(e))
+
+    if res is None:
+        return None
+
+    return res['choices'][0]['message']['content'].replace('<|end_of_turn|>', '') if 'choices' in res else None
+
+
 def processing(queue_item):
     if not queue_item or 'data' not in queue_item:
         print(f'Send error message - Bad data.')
         send_queue_error(queue_item['uuid'], 'Processing error. Bad data.')
         return queue_item
 
-    print(queue_item['data'])
+    if 'question' not in queue_item['data']:
+        print(f'Send error message - Bad data.')
+        send_queue_error(queue_item['uuid'], 'Processing error. Bad data.')
+        return queue_item
+
+    print('---------------------')
+    question = queue_item['data']['question']
+    result = chat_completion(question)
+    if result is not None:
+        print('Question: ' + question)
+        print('Answer: ' + result)
+        res = send_queue_result(queue_item['uuid'], result)
+        print('Completed.')
+    else:
+        print(f'Output is empty. Send error message - Processing error.')
+        send_queue_error(queue_item['uuid'], 'Processing error. Please try again later.')
 
     print('---------------------')
     return queue_item
