4ba398120837d48ad7535e8189e4bba6a6300bd3
andrew <andchir@gmail.com>
2024-06-18 02:00:15 +0300

    Add websocket server

---
+++ web/client.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import sys
+import json
+import asyncio
+import websockets
+
+
+async def send_message(recipient_uuid, message):
+    uri = 'ws://localhost:8765'
+    async with websockets.connect(uri) as websocket:
+        data = {'recipient_uuid': recipient_uuid, 'message': message}
+
+        await websocket.send(json.dumps(data))
+        print('Send:', data)
+
+
+if __name__ == "__main__":
+    recipient_uuid = sys.argv[1] if len(sys.argv) > 1 else None
+    message = sys.argv[2] if len(sys.argv) > 2 else 'Hello.'
+    if recipient_uuid is not None:
+        asyncio.run(send_message(recipient_uuid, message))
+    else:
+        print('Usage: client.py <recipient_uuid> <message>')
---
+++ web/server.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import uuid
+import asyncio
+import json
+import websockets
+
+CONNECTIONS = {}
+
+async def register(websocket):
+    tmp_uuid = str(uuid.uuid1())
+    print('New connection', tmp_uuid)
+    CONNECTIONS['tmp_' + tmp_uuid] = websocket
+    print('Connections total:', len(CONNECTIONS))
+    await websocket.send('..:: Hello from the Notification Center ::..')
+    try:
+        async for message in websocket:
+            event = json.loads(message)
+            recipient_uuid = event['recipient_uuid'] if 'recipient_uuid' in event else None
+            message = event['message'] if 'message' in event else ''
+            if message == 'connected' and recipient_uuid:
+                print('Set UUID:', recipient_uuid)
+                CONNECTIONS[recipient_uuid] = websocket
+                del CONNECTIONS['tmp_' + tmp_uuid]
+            else:
+                print('Message', event)
+                if recipient_uuid is not None and recipient_uuid in CONNECTIONS:
+                    await CONNECTIONS[recipient_uuid].send(message)
+    finally:
+        print('Disconnected', tmp_uuid)
+        if 'tmp_' + tmp_uuid in CONNECTIONS:
+            del CONNECTIONS['tmp_' + tmp_uuid]
+        else:
+            for key, con in CONNECTIONS.items():
+                if con == websocket:
+                    del CONNECTIONS[key]
+                    break
+        print('Connections total:', len(CONNECTIONS))
+
+
+async def main():
+    print('Starting WebSocket server')
+    async with websockets.serve(register, 'localhost', 8765):
+        await asyncio.Future()  # Run forever
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
