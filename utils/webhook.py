fd13cd0a5c539459dbadbeb82d88ade4261409d3
andrew <andchir@gmail.com>
2024-09-18 23:08:38 +0300

    webhook request = allow redirect

--- utils/webhook.py
+++ utils/webhook.py
@@ -9,7 +9,7 @@ def webhook_post_result(webhook_url, queue_uuid, status, result_data):
     }
     webhook_resp = None
     try:
-        r = requests.post(webhook_url, json=webhook_data, allow_redirects=True, timeout=60)
+        r = requests.request('POST', webhook_url, json=webhook_data, allow_redirects=True, timeout=60)
         webhook_resp = r.json()
     except Exception as e:
         print(str(e))
