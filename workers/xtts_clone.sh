ea358309a64d6bb042bec81fb31f39ebb39bbfa0
andrew <andchir@gmail.com>
2024-11-24 23:46:18 +0300

    xtts_clone worker

--- workers/xtts_clone.sh
+++ workers/xtts_clone.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 source "/home/andrew/python_projects/coqui-ai-TTS/venv/bin/activate"
+export TTS_HOME="/home/andrew/python_projects/coqui-ai-TTS/.local"
 
 python /home/andrew/python_projects/coqui-ai-TTS/clone_voice.py "$1" "$2"
 
