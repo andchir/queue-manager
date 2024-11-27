a743d18208961f76d88b1146e8a71a7c76a7bd4f
Andrei Chirko <andchir@gmail.com>
2024-11-27 14:16:46 +0300

    live-portrait worker upd

--- workers/live-portrait.py
+++ workers/live-portrait.py
@@ -51,6 +51,8 @@ def generate_video(image_file_path, driven_video_name=None):
         driving_video_path = '/home/andrew/python_projects/LivePortrait/assets/examples/driving/opera-pavarotti.mp4'
     elif driven_video_name.lower() in ['опера - женский голос', 'opera_female']:
         driving_video_path = '/home/andrew/python_projects/LivePortrait/assets/examples/driving/opera_female.mp4'
+    elif driven_video_name.lower() in ['ёлочка - женский голос', 'elochka_female']:
+        driving_video_path = '/home/andrew/python_projects/LivePortrait/assets/examples/driving/elochka_female.mp4'
     else:
         driving_video_path = random.choice([
             '/home/andrew/python_projects/LivePortrait/assets/examples/driving/marta1.pkl',
