695a616f1c9e936ac3335af6fda707cb47269b99
Andrei Chirko <andchir@gmail.com>
2024-12-03 12:13:46 +0300

    worker manager upd

--- utils/repository.py
+++ utils/repository.py
@@ -62,10 +62,10 @@ class SQLAlchemyRepository(AbstractRepository):
             return None
         return obj
 
-    def find_all(self, limit=100, filter=None, order='desc'):
-        order_opt = self.model.id.desc() if order == 'desc' else self.model.id.asc()
-        stmt = select(self.model).filter_by(**filter).order_by(order_opt).limit(limit) if filter \
-            else select(self.model).order_by(order_opt).limit(limit)
+    def find_all(self, filter=None, limit=100, sort_dir='desc'):
+        sort_dir_opt = self.model.id.desc() if sort_dir == 'desc' else self.model.id.asc()
+        stmt = select(self.model).filter_by(**filter).order_by(sort_dir_opt).limit(limit) if filter \
+            else select(self.model).order_by(sort_dir_opt).limit(limit)
         res = self.session.execute(stmt)
         res = [row[0].to_read_model() for row in res.all()]
         return res
