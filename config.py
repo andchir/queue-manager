1f54da8c67c628d92acbe3e725136ac070a901be
andrew <andchir@gmail.com>
2025-02-01 23:50:43 +0300

    mysql db settings

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ class Settings(BaseSettings):
     admin_email: str = 'aaa@bbb.cc'
     api_keys: str = ''
     sqlite_db_name: str = 'app_database.db'
+    mysql_connection_string: str = 'user:pass@some_mariadb/dbname?charset=utf8mb4'
     max_execution_time: int = 14400
     max_store_time: int = 43200
     gdrive_folder_id: str = ''
--- db/db.py
+++ db/db.py
@@ -2,7 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 from config import settings
 
-engine = create_engine('sqlite:///' + settings.sqlite_db_name)
+if settings.sqlite_db_name:
+    engine = create_engine('sqlite:///' + settings.sqlite_db_name)
+else:
+    engine = create_engine(
+        'mysql+pymysql://' + settings.mysql_connection_string
+    )
 session_maker = sessionmaker(engine)
 
 
