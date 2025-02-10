0664a9e16347e4e1b055b463a62b3ee6bec5722d
andrew <andchir@gmail.com>
2025-02-10 23:08:04 +0300

    db connection upd

--- db/db.py
+++ db/db.py
@@ -6,8 +6,11 @@ if settings.sqlite_db_name:
     engine = create_engine('sqlite:///' + settings.sqlite_db_name)
 else:
     engine = create_engine(
-        'mysql+pymysql://' + settings.mysql_connection_string
+        'mysql+pymysql://' + settings.mysql_connection_string,
         # 'mariadb+pymysql://' + settings.mysql_connection_string
+        pool_recycle=3600,
+        pool_pre_ping=True,
+        echo=True,
     )
 session_maker = sessionmaker(engine)
 
