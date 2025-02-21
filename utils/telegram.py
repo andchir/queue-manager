564b6c9500a9ea3edccaeb6bd226b16b14a2b713
andrew <andrew@merlinsoftware.com>
2025-02-21 11:59:18 +0300

    worker upd

--- utils/telegram.py
+++ utils/telegram.py
@@ -5,6 +5,8 @@ from config import settings
 def send_message_to_bot(message):
     bot_token = settings.tg_bot_token
     chat_id = settings.tg_chat_id
+    if not bot_token or not chat_id:
+        return None
 
     url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
     payload = {
