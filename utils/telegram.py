64b20db5c1be40c19e605bfcf5ec4645b8698dd3
andrew <andchir@gmail.com>
2025-06-01 00:27:44 +0300

    send_message_to_bot upd - split message by chunks

--- utils/telegram.py
+++ utils/telegram.py
@@ -2,16 +2,26 @@ import requests
 from config import settings
 
 
-def send_message_to_bot(message):
+def send_message_to_bot(message, max_length=3072):
     bot_token = settings.tg_bot_token
     chat_id = settings.tg_chat_id
     if not bot_token or not chat_id:
         return None
 
     url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
-    payload = {
-        'chat_id': chat_id,
-        'text': message
-    }
-    response = requests.post(url, json=payload)
-    return response.json()
+    responses = []
+
+    for i in range(0, len(message), max_length):
+        chunk = message[i:i + max_length]
+        payload = {
+            'chat_id': chat_id,
+            'text': chunk
+        }
+        try:
+            response = requests.post(url, json=payload)
+            responses.append(response.json())
+        except requests.RequestException as e:
+            print(f"Error: {e}")
+            return None
+
+    return responses[-1] if responses else None
