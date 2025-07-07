c5fc4b611a0330804de6afdca09bd16eb8dab0f8
andrew <andchir@gmail.com>
2025-07-08 01:13:23 +0300

    upd

--- utils/upload_to_yadisk.py
+++ utils/upload_to_yadisk.py
@@ -19,11 +19,15 @@ def upload_and_share_file(file_path: str, dir_path: str, type='image'):
 
         base_name = os.path.basename(file_path)
         extension = os.path.splitext(base_name)[1]
-        if extension in ['.mp4', '.3gp', '.avi']:
-            result = client.upload(file_path, str(os.path.join(dir_path, base_name.replace(extension, ''))), overwrite=True, timeout=3600)
-            result = client.rename(result.path, base_name, overwrite=True)
-        else:
-            result = client.upload(file_path, f'{dir_path}/{base_name}', overwrite=True, timeout=3600)
+        try:
+            if extension in ['.mp4', '.3gp', '.avi']:
+                result = client.upload(file_path, str(os.path.join(dir_path, base_name.replace(extension, ''))), overwrite=True, timeout=3600)
+                result = client.rename(result.path, base_name, overwrite=True)
+            else:
+                result = client.upload(file_path, f'{dir_path}/{base_name}', overwrite=True, timeout=3600)
+        except Exception as e:
+            print(e)
+            return None, None
 
         if result is None or not hasattr(result, 'path'):
             return None, None
@@ -46,24 +50,36 @@ def upload_and_share_file(file_path: str, dir_path: str, type='image'):
 def delete_old_files_yadisk(dir_path, offset=0, limit=100, max_hours=6):
     now = datetime.datetime.now(datetime.timezone.utc)
     client = yadisk.Client(token=settings.yadisk_token)
-    files_list = list(client.listdir(dir_path, offset=offset, limit=limit, fields=[
-        'path', 'resource_id', 'file', 'size', 'created', 'media_type']))
-    count = 0
-    for item in files_list:
-        time_diff = now - item.created
-        if time_diff.total_seconds() / 60 / 60 > max_hours:
-            client.remove(item.path)
-            count += 1
-    print(f'Deleted {count} files in {dir_path}.')
-    print('Emptying the trash bin...')
-    client.remove_trash('/')
-    print('Success!')
+    with client:
+        # info = client.get_disk_info()
+        # print('trash_size:', info.trash_size)
+        # print('used_space:', info.used_space)
+        # print('total_space:', info.total_space)
+        # print(info)
+        try:
+            files_list = list(client.listdir(dir_path, offset=offset, limit=limit, fields=[
+                'path', 'resource_id', 'file', 'size', 'created', 'media_type'], timeout=30, max_items=limit, n_retries=3))
+        except Exception as e:
+            print(e)
+            files_list = []
+        count = 0
+        print('total: ', len(files_list))
+        for item in files_list:
+            # print(item)
+            time_diff = now - item.created
+            if time_diff.total_seconds() / 60 / 60 > max_hours:
+                client.remove(item.path)
+                count += 1
+        print(f'Deleted {count} files in {dir_path}.')
+        print('Emptying the trash bin...')
+        client.remove_trash('/')
+        print('Success!')
 
 
 if __name__ == '__main__':
     dir_path = 'api2app/media'
-    # file_path = '/media/andrew/KINGSTON/clip-art/wallpapers/animal-gea928f56a_1920.jpg'
-    file_path = '/media/andrew/KINGSTON/video/River - 131339.mp4'
+    file_path = '/media/andrew/KINGSTON/images/rocket.png'
+    # file_path = '/media/andrew/KINGSTON/video/River - 131339.mp4'
     args = sys.argv[1:]
     action = args[0] if len(args) > 0 else ''
     offset = int(args[1]) if len(args) > 1 else 0
